b302e5f639d38d59a915a74a68288d5421257616
axn0684 <148822594+axn0684@users.noreply.github.com>
2024-06-23 19:15:37 +0700

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -23,12 +23,14 @@ for i in range(1, 56):
             generated_path=path_os+'cgan_CEDAR_forg/generated_forgeries_' + s + '_' + s1 + '_' + idx+'.png'
             ssim+=ssim_cal(generated_path,forg) #calculate the ssim of the generated image with the forgery image
             generated.append(preprocess_img(generated_path))
+#save the processed images
 with open(path_os + '/CEDAR_forg.npy', 'wb') as f:
     np.save(f, full_forg, )
 with open(path_os + '/CEDAR_real.npy', 'wb') as f:
     np.save(f, full_real, )
 with open(path_os + '/cgan_CEDAR_forgl.npy', 'wb') as f:
     np.save(f, full_real, )
+print("Average SSIM score: "+str(ssim/(55*24*9))) #print the average SSIM score
 
 
 
