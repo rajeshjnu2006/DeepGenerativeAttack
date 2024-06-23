fe577e65f26add19c3fd017acc2137bd35d78b5c
axn0684 <148822594+axn0684@users.noreply.github.com>
2024-06-23 19:03:07 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import numpy as np
 from skimage.metrics import structural_similarity as ssim
 
 def preprocess_img(img):
+    img = cv2.imread(img)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = img.astype("uint8")
     _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
