9c33c4bca4388b0c557ac2542b98c56d05557804
axn0684 <148822594+axn0684@users.noreply.github.com>
2024-06-26 04:20:21 -0400

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -15,14 +15,15 @@ for i in range(1, 56):
         s = str(i)
         s1 = str(j)
         forg = preprocess_img(path_os + 'full_forg/forgeries_' + s + '_' + s1 + '.png')
-        full_forg.append(ref_img)
+        full_forg.append(cv2.merge[forg,forg,forg]) #convert to RGB
         real = preprocess_img(path_os + 'full_org/original_' + s + '_' + s1 + '.png')
-        full_real.append(preprocess_img(path_os + 'full_org/original_' + s + '_' + s1 + '.png'))
+        full_real.append(cv2.merge([real,real,real]))
         for k in range(9):  # for the generated images
             idx=str(k)
             generated_path=path_os+'cgan_CEDAR_forg/generated_forgeries_' + s + '_' + s1 + '_' + idx+'.png'
             ssim+=ssim_cal(generated_path,forg) #calculate the ssim of the generated image with the forgery image
-            generated.append(preprocess_img(generated_path))
+            generated_img=preprocess_img(generated_path)
+            generated.append(cv2.merge([generated_img,generated_img,generated_img]))
 #save the processed images
 with open(path_os + '/CEDAR_forg.npy', 'wb') as f:
     np.save(f, full_forg, )
